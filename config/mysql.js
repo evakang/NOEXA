1fbd0ac003a3106ef779b89f45ce18c818f44cee
evakang <184970975@qq.com>
2021-01-13 16:36:03 +0800

    one

---
+++ config/mysql.js
@@ -0,0 +1,14 @@
+mysql = {
+
+    host: "127.0.0.1", //这是数据库的地址
+
+    user: "root", //需要用户的名字
+
+    password: "0000", //用户密码 ，如果你没有密码，直接双引号就是
+
+    database: "fmj" //数据库名字
+
+} 
+
+
+module.exports = mysql; //用module.exports暴露出这个接口
